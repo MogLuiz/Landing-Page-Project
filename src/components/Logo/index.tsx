ba67212ab5696bc88e4ba68e9158a345699c09b4
Luiz Henrique <luizhjramos@outlook.com>
2022-03-03 19:30:14 -0300

    feat: get image by url function

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 
 import { LogoProps } from 'types/api'
+import { getImageUrl } from 'utils/getImageUrl'
 
 import * as S from './styles'
 
 const Logo: React.FC<LogoProps> = ({ alternativeText, url }) => (
-  <S.LogoWrapper src={`${url}`} alt={alternativeText} />
+  <S.LogoWrapper src={getImageUrl(url)} alt={alternativeText} />
 )
 
 export default Logo
