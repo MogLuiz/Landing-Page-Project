d1a73b34e1d6feea56e82c3b71bc253b0db5b889
Luiz Henrique <luizhjramos@outlook.com>
2022-03-03 20:32:59 -0300

    feat: Header Graphql Query

--- src/graphql/queries/getLandingPage.ts
+++ src/graphql/queries/getLandingPage.ts
@@ -1,10 +1,33 @@
 const GET_LANDING_PAGE = /*GraphQL */ `
-  query GET_LANDING_PAGE {
-    landingPage {
-      logo {
-        alternativeText
+  fragment image on UploadFile {
+    alternativeText
+    url
+  }
+
+  fragment logo on LandingPage {
+    logo {
+    ...image
+    }
+  }
+
+  fragment header on LandingPage {
+    header {
+      title
+      description
+      button {
+        label
         url
       }
+      image {
+        ...image
+      }
+    }
+  }
+
+  query GET_LANDING_PAGE {
+    landingPage {
+      ...logo
+      ...header
     }
   }
 `
